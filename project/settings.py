bba9a94a050d187671e820403ccbc217be0a9601
Rahul4h <rahul255gh68@gmail.com>
2025-07-17 15:20:29 +0600

    Fix: added allowed hosts for Render

--- project/settings.py
+++ project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-_#tq1cj2i(k)z5!bmku0bb_+ae*a&fl^xjiuvne(#lq1=5a!%y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost','hat.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','hat-t52a.onrender.com',]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://127.0.0.1:8000",
