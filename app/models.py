ee7a56217116dee70923e9b31b10165969c740d4
Rahul4h <rahul255gh68@gmail.com>
2026-04-11 16:20:29 +0600

    remove sqlite and update gitignore

--- app/models.py
+++ app/models.py
@@ -18,6 +18,7 @@ class Contact(models.Model):
 from django.db import models
 
 class Blogs(models.Model):
+    
     CATEGORY_CHOICES = [
         ('shirt', 'Shirt'),
         ('t-shirt', 'T-shirt'),
@@ -38,67 +39,85 @@ class Blogs(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     img = models.ImageField(upload_to='blog_images/')
-    authname = models.CharField(max_length=50)
-    timeStamp = models.DateTimeField(auto_now_add=True)
-    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='tech')
+    authname = models.CharField(max_length=50,db_index=True)
+    timeStamp = models.DateTimeField(auto_now_add=True, db_index=True)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='shirt', db_index=True)
 
     def __str__(self):
         return self.title
+    
+    class Meta:
+        indexes=[models.Index(fields=['category' , 'timeStamp'])]
 
     
 
 class Message(models.Model):
-    sender = models.ForeignKey(User,on_delete=models.CASCADE,related_name='sent_messages')
-    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
+    sender = models.ForeignKey(User,on_delete=models.CASCADE,related_name='sent_messages',db_index=True)
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages', db_index=True)
     content = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField(auto_now_add=True, db_index=True)
     def __str__(self):
         return f'{self.sender}-{self.receiver}:{self.content[:20]}'
+    
+    class Meta:
+        indexes=[models.Index(fields=['sender', 'receiver']),
+                 models.Index(fields=['sender', 'timestamp']),
+                 models.Index(fields=['receiver','timestamp'])]
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(choices=[('salesman', 'Salesman'), ('customer', 'Customer')], max_length=10)
+    role = models.CharField(choices=[('salesman', 'Salesman'), ('customer', 'Customer')], max_length=10,db_index=True)
 
 
 
 
 class Product(models.Model):
     title = models.CharField(max_length=200)
-    category = models.CharField(max_length=100, default='Uncategorized')
+    category = models.CharField(max_length=100, default='Uncategorized', db_index=True)
     image = models.ImageField(upload_to='products/')
     original_price = models.DecimalField(max_digits=10, decimal_places=2)
-    sale_price = models.DecimalField(max_digits=10, decimal_places=2)
-    stock = models.PositiveIntegerField(default=0) 
+    sale_price = models.DecimalField(max_digits=10, decimal_places=2, db_index=True)
+    stock = models.PositiveIntegerField(default=0, db_index=True)
     piece = models.IntegerField(default=0)
 
     def __str__(self):
         return self.title
+    class Meta:
+        indexes=[models.Index(fields=['category', 'sale_price']),
+                 models.Index(fields=['category', 'stock']),]
     
 
 
 class CartItem(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, db_index=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, db_index=True)
     quantity = models.PositiveIntegerField(default=1)
-    added_at = models.DateTimeField(auto_now_add=True)
+    added_at = models.DateTimeField(auto_now_add=True, db_index=True)
 
     def __str__(self):
         return f"{self.user.username} - {self.product.title}"
+    
+    class Meta:
+        indexes=[models.Index(fields=['user', 'product'])]
 
 class Comment(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments')
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments',db_index=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, db_index=True)
     content = models.TextField()
     parent = models.ForeignKey('self', null=True, blank=True, related_name='replies', on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
 
     def __str__(self):
         return f"{self.user.username} on {self.product.title}"
-
-
+    
+    class Meta:
+      indexes = [
+        models.Index(fields=['product', 'created_at']),
+    ]
+    
 
 class ShippingAddress(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, db_index=True)
     address = models.TextField()
     
     phone = models.CharField(
@@ -108,7 +127,7 @@ class ShippingAddress(models.Model):
                 regex=r'^\d{1,11}$',
                 message='Phone number must be numeric and up to 11 digits.'
             )
-        ]
+        ], db_index=True
     )
     
     created_at = models.DateTimeField(auto_now_add=True)
@@ -118,28 +137,34 @@ class ShippingAddress(models.Model):
 
     
 class Order(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, db_index=True)
     shipping_address = models.ForeignKey(ShippingAddress, on_delete=models.SET_NULL, null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True,db_index=True)
     address = models.TextField()  # Optional: You may remove this if you always use shipping_address
-    payment_method = models.CharField(max_length=20)
+    payment_method = models.CharField(max_length=20, db_index=True)
     total = models.DecimalField(max_digits=10, decimal_places=2)
-    is_cancelled = models.BooleanField(default=False)
+    is_cancelled = models.BooleanField(default=False, db_index=True)
     # in Order model
     
     delivery_status = models.CharField(
         max_length=20,
         choices=[('pending', 'Pending'), ('assigned', 'Assigned'), ('delivered', 'Delivered')],
-        default='pending')
+        default='pending', db_index=True)
 
 
     def __str__(self):
         return f"Order #{self.id} by {self.user.username}"
-
+    
+    class Meta:
+     indexes = [
+        models.Index(fields=['user', 'created_at']),
+        models.Index(fields=['user', 'delivery_status']),
+        models.Index(fields=['delivery_status', 'created_at']),
+    ]
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items', db_index=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, db_index=True)
     quantity = models.PositiveIntegerField()
 
     def __str__(self):
@@ -191,14 +216,14 @@ class DeliveryBoy(models.Model):
 
 class DeliveryOrder(models.Model):
     order = models.OneToOneField(Order, on_delete=models.CASCADE, related_name='delivery_info')  # NEW: Link to original Order
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="customer_orders")
-    assigned_to = models.ForeignKey(DeliveryBoy, on_delete=models.SET_NULL, null=True, blank=True, related_name="assigned_orders")
-    status = models.CharField(max_length=20, choices=[('pending', 'Pending'), ('assigned', 'Assigned'), ('delivered', 'Delivered'), ('cancelled', 'Cancelled')], default='pending')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="customer_orders",db_index=True)
+    assigned_to = models.ForeignKey(DeliveryBoy, on_delete=models.SET_NULL, null=True, blank=True, related_name="assigned_orders", db_index=True)
+    status = models.CharField(max_length=20, choices=[('pending', 'Pending'), ('assigned', 'Assigned'), ('delivered', 'Delivered'), ('cancelled', 'Cancelled')], default='pending',db_index=True)
     address = models.TextField()
     total = models.FloatField(default=0)
     payment_method = models.CharField(max_length=50, default='Cash')
     order_image = models.ImageField(upload_to='orders/', null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
     is_cancelled = models.BooleanField(default=False)
     marked_delivered_by_boy = models.BooleanField(default=False)
     confirmed_by_customer = models.BooleanField(default=False)
@@ -206,18 +231,22 @@ class DeliveryOrder(models.Model):
     def __str__(self):
         return f"Delivery for Order #{self.order.id} by {self.user.username}"
 
-
+    class Meta:
+      indexes = [
+        models.Index(fields=['status', 'created_at']),
+        models.Index(fields=['assigned_to', 'status']),
+    ]
 
 
 
 class ReturnRequest(models.Model):
-    order        = models.ForeignKey(Order, on_delete=models.CASCADE)
-    customer     = models.ForeignKey(User, on_delete=models.CASCADE)
+    order        = models.ForeignKey(Order, on_delete=models.CASCADE, db_index=True)
+    customer     = models.ForeignKey(User, on_delete=models.CASCADE, db_index=True)
     amount       = models.DecimalField(max_digits=10, decimal_places=2)
-    status       = models.CharField(max_length=20, default='pending')
+    status       = models.CharField(max_length=20, default='pending',db_index=True)
     assigned_to  = models.ForeignKey(DeliveryBoy, null=True, blank=True, on_delete=models.SET_NULL)
     image = models.ImageField(upload_to='returns/', null=True, blank=True)  # ← Add this line
-    created_at   = models.DateTimeField(auto_now_add=True)
+    created_at   = models.DateTimeField(auto_now_add=True, db_index=True)
     is_collected = models.BooleanField(default=False)
 
    
@@ -237,6 +266,12 @@ class ReturnRequest(models.Model):
     def __str__(self):
         return f"Return #{self.id} for Order #{self.order.id}"
     
+    class Meta:
+     indexes = [
+        models.Index(fields=['customer', 'status']),
+        models.Index(fields=['status', 'created_at']),
+    ]
+    
 from django.db import models
 
 class ProductRecommendation(models.Model):
@@ -248,12 +283,16 @@ class ProductRecommendation(models.Model):
         return f"{self.base_product.title} ➤ {self.recommended_product.title} ({self.confidence:.2f})"
 
 class Recommendation(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    timestamp = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, db_index=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, db_index=True)
+    timestamp = models.DateTimeField(auto_now_add=True, db_index=True)
 
     class Meta:
-        unique_together = ('user', 'product')  # Prevent duplicates
+        unique_together = ('user', 'product')
+        indexes = [
+        models.Index(fields=['user', 'timestamp']),
+        models.Index(fields=['product', 'timestamp']),
+    ]  # Prevent duplicates
 
     def __str__(self):
         return f'Recommendation: {self.user.username} → {self.product.title}'
--- project/settings.py
+++ project/settings.py
@@ -119,13 +119,17 @@ ASGI_APPLICATION = 'project.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+DATABASES={
+    'default':{
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hat_db',
+        'USER': 'hat_user',
+        'PASSWORD': 'mypassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
