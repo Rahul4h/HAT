847703d52bca212e1de2f6215a257dd0e4c71bc6
Rahul4h <rahul255gh68@gmail.com>
2026-05-25 17:35:21 +0600

    updated build.sh

--- app/routing.py
+++
@@ -1,8 +0,0 @@
-# yourapp/routing.py
-
-from django.urls import path
-from . import consumers
-
-websocket_urlpatterns = [
-    path("ws/chat/<str:room_name>/", consumers.ChatConsumer.as_asgi()),
-]
