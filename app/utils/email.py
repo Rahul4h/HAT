910264537827a6aac901ebcbad663911f9e073b2
Rahul4h <rahul255gh68@gmail.com>
2026-06-11 14:58:48 +0600

    implement resend

--- app/utils/email.py
+++ app/utils/email.py
@@ -1,12 +1,24 @@
-from django.core.mail import EmailMessage
+import os
+import resend
 from django.conf import settings
 
+# Resend API Key লোড করা
+resend.api_key = os.environ.get("RESEND_API_KEY")
+
 def send_email(subject, html_content, to_email):
-    email = EmailMessage(
-        subject=subject,
-        body=html_content,
-        from_email=settings.DEFAULT_FROM_EMAIL,
-        to=[to_email],
-    )
-    email.content_subtype = "html"
-    email.send(fail_silently=False)
+    try:
+        params = {
+            # অবশ্যই আপনার কেনা ডোমেইনের ইমেইল ব্যবহার করবেন
+            "from": "HAT Account <no-reply@hat-bd.com>", 
+            "to": [to_email],
+            "subject": subject,
+            "html": html_content,
+        }
+        
+        # ইমেইল পাঠানো
+        response = resend.Emails.send(params)
+        return response
+        
+    except Exception as e:
+        # কোনো কারণে ফেইল করলে এরর থ্রো করবে যা আপনার views.py ক্যাচ করবে
+        raise Exception(f"Resend Email Error: {str(e)}")
--- project/settings.py
+++ project/settings.py
@@ -196,18 +196,10 @@ USE_TZ = True
 
 
 
-#RESEND_API_KEY = os.environ.get("RESEND_API_KEY", "")
+RESEND_API_KEY = os.environ.get("RESEND_API_KEY", "")
 # EMAIL CONFIG (GMAIL SMTP)
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
 
