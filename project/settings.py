de5e8c43297b37cc211b3cae8b43c95f81d79a19
Rahul4h <rahul255gh68@gmail.com>
2026-06-11 11:25:21 +0600

    implement resend

--- project/settings.py
+++ project/settings.py
@@ -196,17 +196,7 @@ USE_TZ = True
 
 
 
-EMAIL_HOST = os.environ.get("EMAIL_HOST", "smtp.gmail.com")
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
-EMAIL_PORT = int(os.environ.get("EMAIL_PORT", "587"))
-EMAIL_USE_TLS = env_bool("EMAIL_USE_TLS", True)
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-DEFAULT_FROM_EMAIL = os.environ.get(
-    "DEFAULT_FROM_EMAIL",
-    f"Rahul from HAT <{EMAIL_HOST_USER}>",
-    
-)
+RESEND_API_KEY = os.environ.get("RESEND_API_KEY", "")
 
 
 
